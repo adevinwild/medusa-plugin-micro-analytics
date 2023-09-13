b80534b156b801dba96ff652a2cb61b817c5279c
adevinwild <68734155+adevinwild@users.noreply.github.com>
2023-09-13 08:26:31 +0200

    WIP

--- src/admin/features/overview/index.tsx
+++ src/admin/features/overview/index.tsx
@@ -1,7 +1,17 @@
-"use client";
+"use client"
+
+import { useAdminCustomQuery } from "medusa-react";
+import MetricCard from "../../components/metric-card";
+import { CheckIcon } from "lucide-react";
 
 const Overview = () => {
-  return <div className="gap-x-small grid grid-cols-3">Overview</div>;
+  const query = useAdminCustomQuery("/analytics/dashboard", ["analytics", "dashboard"])
+  if (!query.data || query.isLoading) return <div>Loading...</div>
+
+  return <div className="gap-x-small grid grid-cols-3">
+    <MetricCard icon={CheckIcon} title="Orders completed" value={query.data.orders.completed} />
+    <MetricCard icon={CheckIcon} title="Orders pending" value={query.data.orders.pending} />
+  </div>;
 };
 
 export default Overview;
